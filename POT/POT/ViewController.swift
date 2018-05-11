3de79f40d596663af33ca0dc6f1e9c1219e61575
Adam Ahrens <adam.aahrens@gmail.com>
2018-05-11 09:42:44 -0500

    Initial App

---
+++ POT/POT/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  POT
+//
+//  Created by Adam Ahrens on 5/11/18.
+//  Copyright © 2018 SwiftMN. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
