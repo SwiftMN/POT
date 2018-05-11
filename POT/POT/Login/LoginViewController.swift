ed9f92825e433bbc4b36b44dae4492a2e97f5d9c
Adam Ahrens <adam.aahrens@gmail.com>
2018-05-11 10:45:51 -0500

    Adding empty Pod file

---
+++ POT/POT/Login/LoginViewController.swift
@@ -0,0 +1,13 @@
+//
+//  LoginViewController.swift
+//  POT
+//
+//  Created by Adam Ahrens on 5/11/18.
+//  Copyright © 2018 SwiftMN. All rights reserved.
+//
+
+import UIKit
+
+final class LoginViewController: UIViewController {
+
+}
---
+++ POT/Pods/Target Support Files/Pods-POT/Pods-POT-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_POT : NSObject
+@end
+@implementation PodsDummy_Pods_POT
+@end
